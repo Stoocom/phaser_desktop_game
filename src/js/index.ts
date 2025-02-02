02be2fc17c024c5065767eea15174732b267982e
Александр Елисеев <a.eliseev@demiusgroup.ru>
2025-02-02 13:10:57 +0300

    fixed images

--- src/js/index.ts
+++ src/js/index.ts
@@ -46,11 +46,11 @@ import * as Phaser from "phaser";
     function preload ()
     {
         console.log('preload');
-        this.load.image('sky', '../assets/images/sky.png');
-        this.load.image('ground', '../assets/images/platform.png');
-        this.load.image('star', '../assets/images/star.png');
-        this.load.image('bomb', '../assets/images/bomb.png');
-        this.load.spritesheet('dude', '../assets/images/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('sky', './assets/images/sky.png');
+        this.load.image('ground', './assets/images/platform.png');
+        this.load.image('star', './assets/images/star.png');
+        this.load.image('bomb', './assets/images/bomb.png');
+        this.load.spritesheet('dude', './assets/images/dude.png', { frameWidth: 32, frameHeight: 48 });
         // this.add.image(400, 300, 'sky');
     }
 
